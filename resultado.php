bc25d39970d1ffa41ead9b1e0fc024ae8f827184
sgomezj87 <sebastian.gomez.j@gmail.com>
2020-06-03 16:57:23 -0500

    form

--- resultado.php
+++ resultado.php
@@ -2,9 +2,10 @@
 include "header.php";
 
 $nombre = $_REQUEST['nombre'];
-echo "El nombre de la persona es";
+echo "El nombre de la persona es"
 echo $_REQUEST['nombre'];
 ?>
+
 <h1> Bienvenido <?php echo "hola" ?> a la página </h1>
 <h1> Bienvenido <?php echo $nombre ?> a la página </h1>
 
@@ -12,9 +13,6 @@ echo $_REQUEST['nombre'];
                 <label for="nombre">Nombre:</label><br>
                 <input type="text" class="form-control" id="nombre" name="nombre" value="<?php echo $nombre ?>"><br>
               </div>
-
-
-
 </div>
 <script src="js/script.js"></script>
 <script src="js/jquery-3.5.0.min.js"></script>
